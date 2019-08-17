2a82422b643eaa2441530ee041771286697ae3b1
heptacode <admin@hyunwoo.kim>
2019-08-18 01:00:48 +0900

    Add router

---
+++ src/views/S2.vue
@@ -0,0 +1,15 @@
+<template>
+  <iframe src="http://mc.sunrin.life:1002" frameborder="0"></iframe>
+</template>
+
+<style>
+    body {
+        margin: 0;
+        background: black;
+        overflow: hidden;
+    }
+    iframe {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
